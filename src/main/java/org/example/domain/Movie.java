c80651078fb131053d448aa0a02a44147cd6dcba
joaquinxtomas <jtppalacios@gmail.com>
2024-11-28 16:07:45 -0300

    first commit

---
+++ src/main/java/org/example/domain/Movie.java
@@ -0,0 +1,44 @@
+package org.example.domain;
+
+import java.util.Objects;
+
+public class Movie {
+    private String name;
+
+    public Movie() {
+    }
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+        return Objects.equals(name, movie.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+}
